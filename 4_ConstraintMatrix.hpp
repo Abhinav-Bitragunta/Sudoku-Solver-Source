a8f2d0adf38fbf4f98a2b335e58f6b65ac82182c
Abhinav B <abhinavbitragunta@gmail.com>
2024-11-02 14:32:58 +0530

    Optimized cantPlaceHere() by removing it, and replacing with Board unsafe() function

--- 4_ConstraintMatrix.hpp
+++ 4_ConstraintMatrix.hpp
@@ -11,37 +11,30 @@ class ConstraintMatrix {
     size_t              numRows;
     size_t              gridSize;
 
+    inline void         set             (const size_t& row, const size_t& col, bool val);
+    
    public:
     void                construct       (Board& b);
-    inline void         set             (const size_t& row, const size_t& col, bool val);
     inline bool         at              (const size_t& row, const size_t& col);
-    bool                cantPlaceHere   (const size_t& row, const size_t& col, size_t& val);
     size_t              getRows         ();
     size_t              getCols         ();
 };
 
 void ConstraintMatrix::construct(Board& b) {
     size_t n        = b.rowlength();
-    
+
     this->gridSize  = std::sqrt(n);
 
     this->numCols   = 4 * n * n;
     this->numRows   = n * n * n;
 
     this->data      = std::vector<bool>(this->numCols * this->numRows, false);
-    this->clues     = std::vector<std::vector<int>>();
-    
-    for(int row = 0; row < n; row++) {
-        for(int col = 0; col < n; col++) {
-            if(b.at(row,col))  clues.emplace_back(std::vector<int>{b.at(row, col), row, col});
-        }
-    }
 
     //0-80: row-col, 81-161: row-num, 162-242: col-num, 243-323: grid-num
     for(size_t row = 0; row < n; row++) {
         for(size_t col = 0; col < n; col++) {
             for(size_t val = 0; val < n; val++) {
-                if(cantPlaceHere(row,col,val)) continue;
+                if(b.unsafe(row,col,val+1)) continue;
 
                 size_t  rowidx  = row * n * n + col * n + val,           // rows are 0-728, represent in base 9 as (row)(col)(val)
                         gridnum = (row / gridSize) * gridSize + (col / gridSize),
@@ -58,22 +51,6 @@ void ConstraintMatrix::construct(Board& b) {
             }
         }
     }
-}
-
-bool ConstraintMatrix::cantPlaceHere(const size_t& row, const size_t& col, size_t& val) {
-    if(this->clues.empty()) return false;
-
-    for(const auto& clue : this->clues) {
-        
-        size_t  v = clue[0] - 1,
-                r = clue[1],
-                c = clue[2],
-                gridNumInput    = (row / this->gridSize) * this->gridSize + (col / this->gridSize),
-                gridNumClue     = (r   / this->gridSize) * this->gridSize + (c   / this->gridSize);
-
-        if((v == val || (r == row && c == col)) && (r == row && c == col && gridNumClue == gridNumInput) && !(r == row && v == val && c == col)) return true;
-    }
-    return false;
 }
 
 inline void ConstraintMatrix::set(const size_t& row, const size_t& col, bool val)  {   this->data[row * this->numCols + col] = val;    }
