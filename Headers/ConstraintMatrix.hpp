63d21375d8caef1c21813f31bb206af1015310b2
Abhinav B <abhinavbitragunta@gmail.com>
2024-11-16 13:01:30 +0530

    Added explanation for memory usage.

--- Headers/ConstraintMatrix.hpp
+++ Headers/ConstraintMatrix.hpp
@@ -4,7 +4,7 @@
 
 class ConstraintMatrix {
    private:
-    std::vector<bool>   data;
+    std::vector<bool>   data;           //boolean vector chosen as it uses bitfields to store bools in 1 bit each unlike other linear data structures that use 1 byte each.
     size_t              numCols;
     size_t              numRows;
     size_t              gridSize;
--- src/ToroidalLinkedList.cpp
+++ src/ToroidalLinkedList.cpp
@@ -89,3 +89,16 @@ ToroidalLinkedList::~ToroidalLinkedList() {
     std::cout << std::format("\nHeap memory freed. {} column nodes, {} vertical list nodes, and {} head node for a total of {} nodes destroyed.\n", colCount, verticalCount, headCount, colCount + verticalCount + headCount);
     std::cout << std::format("Memory taken by the data structures: {:.2f} KB.\n", ((headCount+colCount)*sizeof(ColumnNode) + verticalCount*sizeof(Node) + (n*n*n*n*n*0.5f))/1024.0);
 }
+
+/*
+        Memory usage calculation:
+            Since the constraint matrix and toroidal linked list have a space complexity of O(n^5), and since everything else is limited to O(n^2) max, we're only counting those both.
+
+            The cryptic "(n*n*n*n*n*0.5f)" is the memory used in bytes by the constraint matrix.
+            It has n^3 rows and 4n^2 columns, for a total of 4n^5 booleans stored.
+            std::vector<bool> uses only one bit to store a boolean value (using bitfields) unlike the standard 1 byte taken by bool.
+            Therefore, 4n^5 bits * 1/8 = 0.5*n^5 bytes.
+
+            The rest should be self explanatory.
+
+*/
